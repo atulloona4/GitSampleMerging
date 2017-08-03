5bb48b8e53a4763d0e39aaf8db281f829f399c53
atulloona4 <atulloona4@gmail.com>
2017-08-03 12:38:46 +0530

    Initial checkin

---
+++ RingsMagnet/Assets/Engulf/Engulfer.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Engulfer : MonoBehaviour {
+
+    public Transform U100;
+    public Transform U75;
+
+    public Transform U50;
+    public Transform U25;
+    // Use this for initialization
+    void Start () {
+
+        for (int i = 0; i < 10; i++)
+        {
+
+            Instantiate(U100, new Vector3(Mathf.Floor( Random.Range(-5.0f, 5.0f)),0, Mathf.Floor(Random.Range(-5.0f, 5.0f))), Quaternion.AngleAxis(90, new Vector3(0,1,0)));
+            Instantiate(U75, new Vector3(Mathf.Floor(Random.Range(-5.0f, 5.0f)), 0, Mathf.Floor(Random.Range(-5.0f, 5.0f))), Quaternion.identity);
+            Instantiate(U50, new Vector3(Mathf.Floor(Random.Range(-5.0f, 5.0f)), 0, Mathf.Floor(Random.Range(-5.0f, 5.0f))), Quaternion.AngleAxis(90, new Vector3(0, 1, 0)));
+            Instantiate(U25, new Vector3(Mathf.Floor(Random.Range(-5.0f, 5.0f)), 0, Mathf.Floor(Random.Range(-5.0f, 5.0f))), Quaternion.identity);
+        }
+    }
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
---
+++ RingsMagnet/Assets/Map.cs
@@ -0,0 +1,52 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PositionsCoordinates
+{
+   public float x;
+    public float y;
+    public float z;
+}
+
+public class Map : MonoBehaviour {
+
+    public Transform hexa;
+
+    private int[,,] myArray = new int[,,] { };
+
+    List<PositionsCoordinates> Coordinates = new List<PositionsCoordinates>();
+    // Use this for initialization
+    void Start()
+    {
+        float altitude = 1.73205080757F;
+        Coordinates.Add(new PositionsCoordinates() { x = altitude, y = 0, z = 3F });
+
+        Coordinates.Add(new PositionsCoordinates() { x = -altitude, y = 0, z = 3F });
+        Coordinates.Add(new PositionsCoordinates() { x = -2*altitude, y = 0, z = 0 });
+        Coordinates.Add(new PositionsCoordinates() { x = -altitude, y = 0, z = -3F });
+        Coordinates.Add(new PositionsCoordinates() { x = altitude, y = 0, z = -3F });
+        Coordinates.Add(new PositionsCoordinates() { x = 2*altitude, y = 0, z = 0 });
+
+
+        //1->  altitude,0, 3F
+        //2-> - altitude,0 3f
+
+
+
+
+        foreach (PositionsCoordinates value in Coordinates)
+            {
+                //if(hexa != null)
+
+                Instantiate(hexa, new Vector3(hexa.transform.localPosition.x + value.x, hexa.transform.localPosition.y + value.y, hexa.transform.localPosition.z + value.z), Quaternion.identity);
+            }
+
+        //var clone  = Instantiate (player, player.transform);
+
+
+        //.< Renderer > ().bounds.size
+        // player.transform.RotateAround(player1.transform.position, Vector3.zero, 50);
+
+    }
+}
